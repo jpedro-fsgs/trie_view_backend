135c4590609c0be27002f855601f2757a7e719b5
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-02-13 21:36:32 -0300

    feat: trie logic

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from trie import Trie
 
 app = FastAPI()
 
-trie = Trie()
+allowed_origins = os.environ.get("ALLOWED_ORIGINS", "").split(',')
+clear_password = os.environ.get("CLEAR_PASSWORD", "")
 
-@app.get("/")
-async def hello():
-    return "Hello"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
+
+public_trie = Trie()
+
+palavras_trie = Trie()
+
+with open('data/palavras', 'r') as file:
+    words_list = file.read().split('\n')
+
+for word in words_list:
+    palavras_trie.insert(word)
+
+@app.get("/match/{word}")
+def match(word: str):
+    results = palavras_trie.matches(word)
+    return {
+        "length": len(results),
+        "results": results
+    }
+
+@app.post("/insert/{word}")
+def insert(word: str):
+    if word == clear_password:
+        public_trie.clear()
+        return
+    
+    public_trie.insert(word.strip())
+
+
+@app.get("/tree")
+def generate_tree():
+    return public_trie.get_tree()
--- trie.py
+++ trie.py
@@ -8,6 +8,9 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
 
+    def clear(self):
+        self.root = TrieNode()
+
     def insert(self, word):
         node = self.root
         for char in word:
@@ -16,6 +19,7 @@ class Trie:
             node = node.children[char]
         node.word = word
 
+
     def search(self, word):
         node = self.root
         for char in word:
@@ -23,7 +27,7 @@ class Trie:
                 return False
             node = node.children[char]
         return node.word
-    
+
     def matches(self, word):
         node = self.root
         for char in word:
@@ -31,51 +35,31 @@ class Trie:
                 return []
             node = node.children[char]
 
-        def get_matches(node):
-            result = [child_node.word for child_node in node.children.values() if child_node.word]
-            for child_node in node.children.values():
-                result += get_matches(child_node)
-            return result
+        matches_list = []
+        queue = []
+        queue.append(node)
 
-        return get_matches(node)
+        while queue:
+            if len(matches_list) > 50:
+                return matches_list
 
-        
+            current_node = queue.pop()
+            if current_node.word:
+                matches_list.append(current_node.word)
+            queue.extend(current_node.children.values())
 
+        return matches_list
 
+    def get_tree(self):
 
-    # def get_tree(self):
-    #     def build_tree(node):
-    #         tree = {"name": "Root", "children": []}
-    #         for char, child_node in node.children.items():
-    #             subtree = {
-    #                 "name": char,
-    #                 "is_end_of_word": node.is_end_of_word,
-    #                 "children": build_tree(child_node)["children"],
-    #             }
-    #             tree["children"].append(subtree)
-    #         return tree
+        def build_tree(node):
+            return [
+                {
+                    "name": char,
+                    "children": build_tree(child_node),
+                    **({"attributes": {"word": child_node.word}} if child_node.word else {})
+                }
+                for char, child_node in node.children.items()
+            ]
 
-    #     return build_tree(self.root)
-
-
-# Exemplo de uso:
-trie = Trie()
-trie.insert("chat")
-trie.insert("chatbot")
-trie.insert("chatt")
-trie.insert("rato")
-trie.insert("coelho")
-trie.insert("coe")
-
-print(trie.search("rato"))
-print(trie.search("coelho"))
-print(trie.search("coe"))
-print(trie.search("r"))
-print(trie.search("ra"))
-
-print(trie.matches("co"))
-
-# import json
-
-# tree_json = json.dumps(trie.get_tree(), indent=2)
-# print(tree_json)
+        return {"name": "Root", "children": build_tree(self.root)}
